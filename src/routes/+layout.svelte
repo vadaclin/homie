7fecbd5e324171d02c69e1ff2fc0b96ad75fdb2b
vadaclin <vadaclin@students.zhaw.ch>
2026-04-29 15:50:23 +0200

    changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,25 +1,55 @@
 <script>
+  import "bootstrap/dist/css/bootstrap.min.css";
   let { children } = $props();
 </script>
 
-<nav>
-  <a href="/">Home</a>
-  <a href="/vorrat">Vorrat</a>
-  <a href="/einkaufsliste">Einkaufsliste</a>
+<nav class="navbar navbar-expand-lg custom-nav">
+  <div class="container">
+    <a class="navbar-brand fw-bold" href="/"> Homie </a>
+
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#nav"
+    >
+      ☰
+    </button>
+
+    <div class="collapse navbar-collapse" id="nav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/vorrat">Vorrat</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/einkaufsliste">Einkaufsliste</a>
+        </li>
+      </ul>
+    </div>
+  </div>
 </nav>
 
 {@render children()}
 
 <style>
-  nav {
-    display: flex;
-    gap: 1rem;
-    padding: 1rem;
-    background: #111827;
+  .custom-nav {
+    background: #faf7f5;
+    border-bottom: 1px solid #e8e2dd;
   }
 
-  a {
-    color: white;
-    text-decoration: none;
+  .navbar-brand {
+    color: #d97757;
+  }
+
+  .nav-link {
+    color: #2b2b2b;
+    font-weight: 500;
+  }
+
+  .nav-link:hover {
+    color: #d97757;
   }
 </style>
